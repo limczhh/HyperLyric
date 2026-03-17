2e6e52d64011d837084a72faf2e91668139795a0
lidesheng <czhh2436@gmail.com>
2026-03-17 18:17:26 +0800

    更新v4.8.1,新增工作流

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1921
-        versionName = "4.8"
+        versionName = "4.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
